96c757f0a3f2e74448f9caac53c61102bf94c046
Vania Natalie <71621406+VaniaNatalie@users.noreply.github.com>
2021-05-17 16:27:46 +0700

    Update hashtable.cpp

--- bank/hashtable.cpp
+++ bank/hashtable.cpp
@@ -32,32 +32,29 @@ void HashTable::insertItem(int key, string name, string password, int pin, doubl
 	hashTable[hashValue].push_back(val);
 }
 
+
 // Delete item from hash tables by key
 void HashTable::deleteItem(int key) {
-	hashValue = hashFunction(key);
-	for (auto it: hashTable) {
-		if (it.first == hashValue) {
-			hashTable.erase(hashValue);
-			cout << "DELETED" << endl;
-		}
-		else {
-			cout << "[ERROR] No ID found" << endl;
-		}
-	}
+	
 }
 
-// Display the hash tables
-void HashTable::displayHash() {	
-	for (auto it : hashTable) {
-		cout << "ID: " << it.second.key << endl;
-		cout << "Name: " << it.second.name << endl;
-		cout << "Password: " << it.second.password << endl;
-		cout << "Pin Number: " << it.second.pin << endl;
-		cout << "Balance: " << it.second.balance << endl;
-	}   	   	
+
+// Search hashtable
+void HashTable::searchHash(int key) {	
+	hashValue = hashFunction(key);
+	for (auto x : hashTable[hashValue]) {
+		if (x.key == key) {
+			cout << "ID: " << x.key << endl;
+			cout << "Name: " << x.name << endl;
+			cout << "Balance: $" << x.balance << endl;
+			return;
+		}
+	}
+	cout << "[ERROR] ID " << key << " not found";
+	return;	
 }
+
 
 int HashTable::hashFunction(int key) {
 	return key % 10;
 }
-
